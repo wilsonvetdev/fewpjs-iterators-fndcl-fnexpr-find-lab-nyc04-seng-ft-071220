231fdb12983dcbab16a9c47487538891b9e04b82
wilsonng21 <wilsongch92@gmail.com>
2020-09-15 19:57:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+function superbowlWin(records) {
+  let result = records.find( function(record) {
+    return record.result === "W"
+  })
+
+  if(!!result) 
+    { return result.year }  
 }
+
